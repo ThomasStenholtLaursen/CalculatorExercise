81900853ba69fe102436b76fd043ff13d7a5f06b
Samuel Lund Jessen <au646967@uni.au.dk>
2022-02-09 15:42:18 +0100

    added subtract test

--- Calculator.Test.Unit/CalculatorTest.cs
+++ Calculator.Test.Unit/CalculatorTest.cs
@@ -26,6 +26,17 @@ namespace Calculator.Test.Unit
             Assert.That(test, Is.EqualTo(result));
         }
 
+        [TestCase(10, 5, 5)]
+        [TestCase(14, 7, 7)]
+        [TestCase(6, 9, -3)]
+        [TestCase(15.6, 8.9, 6.7)]
+        [Test]
+        public void TestOfSubtract_MulitpleNumbes(double a, double b, double result)
+        {
+            var test = _uut.Subtract(a, b);
+            Assert.That(test, Is.EqualTo(result));
+        }
+
         [TestCase(2, 2, 4)]
         [TestCase(5, 2, 25)]
         [TestCase(5, -1, 0.2)]
